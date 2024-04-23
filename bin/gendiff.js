15b136583cd7362c958204001b2d2c164edeff16
Nataliya Rodikova <bachininang@gmail.com>
2024-04-23 12:21:36 +0500

    step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command('gendiff');
 
@@ -13,7 +14,7 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(filepath1, filepath2);
+    console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import _ from 'lodash';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+
+const getPath = (filename) => path.resolve(cwd(), filename);
+
+const getFileFormat = (filename) => path.extname(filename).slice(1);
+
+const readFile = (filepath) => readFileSync(filepath, 'utf8');
+
+const parseJson = (file) => JSON.parse(readFile(getPath(file)));
+
+const genDiff = (filepath1, filepath2) => {
+  const json1 = parseJson(filepath1);
+  const json2 = parseJson(filepath2);
+
+  const sortedAllKey = _.sortBy(_.union(Object.keys(json1), Object.keys(json2)));
+  const diffsArray = sortedAllKey.map((key) => {
+    const isKeyInJson1 = _.has(json1, key);
+    const isKeyInJson2 = _.has(json2, key);
+    if (isKeyInJson1 && !isKeyInJson2) {
+      return `  - ${key}: ${json1[key]}`;
+    }
+    if (!isKeyInJson1 && isKeyInJson2) {
+      return `  + ${key}: ${json2[key]}`;
+    }
+    if (isKeyInJson1 && isKeyInJson2 && json1[key] === json2[key]) {
+      return `    ${key}: ${json1[key]}`;
+    }
+    return `  - ${key}: ${json1[key]}\n  + ${key}: ${json2[key]}`;
+  });
+  return ['{', ...diffsArray, '}'].join('\n');
+};
+
+export default genDiff;
