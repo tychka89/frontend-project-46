31a6c422767f4d617a19a47a71c0987544f55af1
Nataliya Rodikova <bachininang@gmail.com>
2024-05-01 19:49:15 +0500

    10

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -5,7 +5,7 @@ const formatValueToPlain = (value) => {
   if (_.isPlainObject(value)) {
     return '[complex value]';
   }
-  if (_.isString(value) {
+  if (_.isString(value)) {
     return `'${value}'`;
   }
   return `${value}`;
